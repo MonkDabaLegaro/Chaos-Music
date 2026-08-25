fed2535c2b19b0041a718e4881f7ddde5ed74a85
MonkDabaLegaro <monk.of.the.chaos@gmail.com>
2026-08-25 14:19:24 -0400

    test: run component and hook suites in jsdom

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('jest').Config} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   roots: ['<rootDir>/packages', '<rootDir>/apps/desktop'],
   testMatch: [
     '**/__tests__/**/*.test.(ts|tsx)',
