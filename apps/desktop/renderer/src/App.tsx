6f325978336cf0e567745ba869bea8f1cebc13e1
MonkDabaLegaro <monk.of.the.chaos@gmail.com>
2026-08-25 12:32:05 -0400

    refactor: simplify renderer composition root

--- apps/desktop/renderer/src/App.tsx
+++ apps/desktop/renderer/src/App.tsx
@@ -1,23 +1,13 @@
-import { useEffect } from 'react';
-import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainLayout from './components/layout/MainLayout';
 import { useKeyboardShortcuts } from './hooks/useKeyboardShortcuts';
-import { useTheme } from './hooks/useTheme';
 import ExplorePage from './pages/ExplorePage';
 import HomePage from './pages/HomePage';
 import LibraryPage from './pages/LibraryPage';
 import SearchPage from './pages/SearchPage';
-import { store } from './store';
 
-function AppContent() {
-  const { theme } = useTheme();
+function AppRoutes() {
   useKeyboardShortcuts();
-
-  useEffect(() => {
-    // Initialize app
-    console.log('App initialized with theme:', theme);
-  }, [theme]);
 
   return (
     <MainLayout>
@@ -25,21 +15,17 @@ function AppContent() {
         <Route path="/" element={<HomePage />} />
         <Route path="/explore" element={<ExplorePage />} />
         <Route path="/library" element={<LibraryPage />} />
-        <Route path="/search" element={<SearchPage />} />
         <Route path="/library/:tab" element={<LibraryPage />} />
+        <Route path="/search" element={<SearchPage />} />
       </Routes>
     </MainLayout>
   );
 }
 
-function App() {
+export default function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <AppContent />
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
-
-export default App;
